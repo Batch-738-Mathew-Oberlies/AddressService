7f455861dba50a1c4e340b17b739dc9e6575d2d0
Gilbert Turner <grturner@5x5code.com>
2020-05-01 19:36:27 -0400

    Corrected Mappings and properties files Corrected the GetMapping at the class level of AddressController.java to correct that "/address" is no longer needed. Added a default application.properties as well as a bootstrap.properties that will result in the application.properties to be overwritten with the correct values for datasource info at runtime by kubernetes. Updated the gitignore to remove duplicate entries.

--- src/main/java/com/revature/rideshare/addressservice/controllers/AddressController.java
+++ src/main/java/com/revature/rideshare/addressservice/controllers/AddressController.java
@@ -15,7 +15,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/address")
+@RequestMapping
 @CrossOrigin
 @Api(tags = {"Address"})
 public class AddressController {
