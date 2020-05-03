74933fd1fb3aa5d99f66889cd16ed6c58b90df4e
Gilbert Turner <grturner@5x5code.com>
2020-05-03 13:49:39 -0400

    AOP & Logging Integrated AOP for logging business logic. Configured log4j2 for centralized logging on an external server.

--- src/main/java/com/revature/rideshare/addressservice/AddressServiceApplication.java
+++ src/main/java/com/revature/rideshare/addressservice/AddressServiceApplication.java
@@ -2,8 +2,10 @@ package com.revature.rideshare.addressservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class AddressServiceApplication {
 
 	public static void main(String[] args) {
